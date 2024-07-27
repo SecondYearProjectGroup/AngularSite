e3638d3b531646ffef54490ba28b19ffc1d1d4a2
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2024-07-28 00:23:20 +0530

    Right sidebar

--- src/app/Components/sidebar-right/sidebar-right.component.ts
+++ src/app/Components/sidebar-right/sidebar-right.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-right',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './sidebar-right.component.css'
 })
 export class SidebarRightComponent implements OnInit {
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.initializeSidebar();
@@ -20,12 +22,19 @@ export class SidebarRightComponent implements OnInit {
       hideSidebarBtn?.addEventListener('click', () => {
         sidebar?.classList.add('hidden');
         showSidebarBtn!.style.display = 'block';
+        this.dispatchResizeEvent('full');
       });
 
       showSidebarBtn?.addEventListener('click', () => {
         sidebar?.classList.remove('hidden');
         showSidebarBtn!.style.display = 'none';
+        this.dispatchResizeEvent('reduced');
       });
     });
+  }
+
+  private dispatchResizeEvent(size: 'full' | 'reduced'): void {
+    const event = new CustomEvent('middleContentResize', { detail: size });
+    window.dispatchEvent(event);
   }
 }
