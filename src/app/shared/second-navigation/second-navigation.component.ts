944d52a4411c9f8f8ba9f3a20f2e086142e3ac4c
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2024-08-06 15:29:07 +0530

    08/06

--- src/app/shared/second-navigation/second-navigation.component.ts
+++ src/app/shared/second-navigation/second-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-second-navigation',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class SecondNavigationComponent {
 
+  dropdowns = [
+    { title: 'Computer Engineering', item1: 'Facilities', item2: 'Specializations', item3: 'About', isOpen: false },
+    { title: 'Postgraduate Studies', item1: 'Researches', item2: 'Research Facilities', item3: 'Procedure', isOpen: false },
+    { title: 'Centers & Units', item1: 'Computer Engineering Department', item2: 'Center for Engineering Research', item3: 'Computing Centre', isOpen: false },
+    { title: 'Department & Staff', item1: 'Student Help guide', item2: 'Department ADPC', item3: 'IQAC', isOpen: false }
+  ];
+
+  toggleDropdown(dropdown: any): void {
+    this.dropdowns.forEach(dd => {
+      if (dd !== dropdown) {
+        dd.isOpen = false;
+        console.log("Clicked");
+      }
+    });
+    dropdown.isOpen = !dropdown.isOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdowns(event: MouseEvent): void {
+    if (!(event.target as HTMLElement).closest('.myDropdown')) {
+      this.dropdowns.forEach(dropdown => {
+        dropdown.isOpen = false;
+        console.log("Clicked Outside");
+      });
+    }
+  }
 }
