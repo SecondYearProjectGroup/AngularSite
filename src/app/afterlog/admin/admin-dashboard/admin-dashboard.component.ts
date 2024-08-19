5a7990e44aedead4034243661bca593bf6548675
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2024-08-19 20:35:18 +0530

    19/8/2024 8.35pm

--- src/app/afterlog/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/afterlog/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthServiceService } from '../../../services/auth-service.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AdminDashboardComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService : AuthServiceService) {}
 
   navigateToEnrolledStudents(){
     this.router.navigate(['afterlog/enrolled-students']);
@@ -27,7 +28,12 @@ export class AdminDashboardComponent {
   }
 
   testingButton(){
-    console.log('Testing Button Clicked.');
+    // this.authService.getCurrentUser().subscribe(
+    //   (responce : string ) => {
+    //     console.log('User' , responce)
+    //   }
+    // )
+    // console.log('Testing Button Clicked.');
   }
 
 
