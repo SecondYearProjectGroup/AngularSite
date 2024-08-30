23999b808c07ce7199ab8c9a2613f643db98ce17
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2024-08-30 09:35:39 +0530

    File download Service

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class FileService {
 
-  private baseUrl = 'http://localhost:8080/files';
+  private baseUrl = 'http://localhost:8080/files/download';
 
 
   constructor(private http: HttpClient) { }
@@ -20,4 +20,17 @@ export class FileService {
   downloadFile(id: number): Observable<Blob> {
     return this.http.get(`${this.baseUrl}/download/${id}`, { responseType: 'blob' });
   }
+
+  downloadFileFromEnrolldStu(fileName: string) {
+    const token = localStorage.getItem('authToken'); // Get the JWT token from localStorage
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(`${this.baseUrl}/${fileName}`, {
+      headers: headers,
+      responseType: 'blob' as 'json' // The response type is 'blob' since it's a file download
+    });
+  }
+  
+
+
 }
