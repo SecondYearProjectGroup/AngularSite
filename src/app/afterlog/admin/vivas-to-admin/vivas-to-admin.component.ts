fd196b585e3d9b15af99202d016d7a88e34dc1d5
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2024-09-15 21:01:53 +0530

    Load VIVA data

--- src/app/afterlog/admin/vivas-to-admin/vivas-to-admin.component.ts
+++ src/app/afterlog/admin/vivas-to-admin/vivas-to-admin.component.ts
@@ -10,36 +10,47 @@ export class VivasToAdminComponent {
 
   constructor(private http: HttpClient){}
 
-  // tableData: Array<{ id: number, fullName: string, department:string, email:string }> = [];
+  tableData: Array<{ 
+    title: string, 
+    vivaDate: Date,
+    //comments: string, 
+    regNumber: string,
+    registrationNumber: string,
+    status: string, 
+  }> = [];
 
   searchText: string = '';
 
   ngOnInit(): void {
     scrollTo(0,0);
-    // this.loadExaminers();
+    this.loadVivas();
   }
   
-  // loadExaminers() {
-  //   this.http.get<Array<{ id: number, fullName: string, department:string, email:string }>>('http://localhost:8080/examinersToAdmin')
-  //     .subscribe({
-  //       next: (data) => {
-  //         this.tableData = data;
-  //       },
-  //       error: (error) => {
-  //         console.error('Error loading supervisor data', error);
-  //       },
-  //       complete: () => {
-  //         console.log('Examiner data loading complete');
-  //       }
-  //     });
-  //   }
+  loadVivas() {
+    this.http.get<Array<{     title: string, 
+      vivaDate: Date,
+      //comments: string, 
+      regNumber: string,
+      registrationNumber: string,
+      status: string,}>>('http://localhost:8080/viva/details')
+      .subscribe({
+        next: (data) => {
+          this.tableData = data;
+        },
+        error: (error) => {
+          console.error('Error loading supervisor data', error);
+        },
+        complete: () => {
+          console.log('Examiner data loading complete');
+        }
+      });
+    }
 
-  // get filteredData() {
-  //   return this.tableData.filter(row =>
-  //     row.id.toString().toLowerCase().includes(this.searchText.toLowerCase()) ||
-  //     row.fullName.toLowerCase().includes(this.searchText.toLowerCase()) ||
-  //     row.department.toLowerCase().includes(this.searchText.toLowerCase()) ||
-  //     row.email.toLowerCase().includes(this.searchText.toLowerCase()) 
-  //   );
-  // }
+    get filteredData() {
+      return this.tableData.filter(row => 
+        (row.regNumber?.toLowerCase().includes(this.searchText.toLowerCase()) || '') ||
+        (row.status?.toLowerCase().includes(this.searchText.toLowerCase()) || '') ||
+        (row.title?.toLowerCase().includes(this.searchText.toLowerCase()) || '')
+      );
+    }
 }
